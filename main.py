cc8d631f22f50ce4c1e6569a8155606beb2e8932
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-19 15:59:24 +0500

    testing and add docs

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import yt_dlp as youtube_dl
 
 
 class YouTubeDownloader:
-    def __init__(self, url_list: list[str], base_path=None):
+    def __init__(self, 
+                 url_list: list[str], 
+                 base_path=None, 
+                 base_quiet=False
+        ):
+
         self.url_list = url_list
         self.base_path = base_path
+        self.base_quiet = base_quiet
 
     def download_video(self, path="download/videos/", format="best") -> None:
+        """
+        Загрузка видео.
+        """
+
         path = path + "%(title)s.%(ext)s"
         ydl_opts = {}
 
         ydl_opts['format'] = format
+        ydl_opts['quiet'] = self.base_quiet
 
         if self.base_path is not None:
-            ydl_opts["outtmpl"] = self.base_path
+            ydl_opts['outtmpl'] = self.base_path
         else:
             ydl_opts['outtmpl'] = path
 
@@ -27,6 +38,10 @@ class YouTubeDownloader:
             print("Successfully downloaded.")
 
     def get_info_dict(self) -> list[dict]:
+        """
+        Извлечение метаданных.
+        """
+
         info_list = []
 
         ydl_opts = {
@@ -44,11 +59,16 @@ class YouTubeDownloader:
         return info_list
 
     def download_audio(self, path="download/audios/", format="bestaudio/best") -> None:
+        """
+        Загрузка аудио.
+        """
+
         path = path + "%(title)s.%(ext)s"
         ydl_opts = {}
 
         ydl_opts['format'] = format
         ydl_opts['extractaudio'] = True
+        ydl_opts['quiet'] = self.base_quiet
 
         if self.base_path is not None:
             ydl_opts["outtmpl"] = self.base_path
@@ -66,19 +86,29 @@ class YouTubeDownloader:
             print("Successfully downloaded.")
 
     def download_playlist(self, path="download/playlists/", format="best") -> None:
+        """
+        Загрузка плейлиста.
+        """
+
         path + path + "%(playlist)s/%(title)s.%(ext)s"
         ydl_opts = {}
 
-        ydl_opts["format"] = format
+        ydl_opts['format'] = format
+        ydl_opts['quiet'] = self.base_quiet
 
         if self.base_path is not None:
             ydl_opts["outtmpl"] = self.base_path
         else:
             ydl_opts['outtmpl'] = path
-      
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(self.url_list)
 
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(self.url_list)
+        except Exception as e:
+            print(e)
+
+        else:
+            print("Successfully downloaded.")
 
 if __name__=="__main__":
 
@@ -89,4 +119,8 @@ if __name__=="__main__":
         ]
     )
 
-    print(downloader.get_info_dict())
+    # print(downloader.get_info_dict())
+
+    # downloader.download_video()
+    # downloader.download_audio()
+    # downloader.download_playlist()
