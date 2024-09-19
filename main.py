76f6230ab5d65df03a8c82d5805a820a404b391c
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-19 16:00:49 +0500

    refactoring code

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import yt_dlp as youtube_dl
 
 
 class YouTubeDownloader:
-    def __init__(self, 
-                 url_list: list[str], 
-                 base_path=None, 
-                 base_quiet=False
-        ):
+    def __init__(self, url_list: list[str], base_path=None, base_quiet=False):
 
         self.url_list = url_list
         self.base_path = base_path
@@ -20,13 +16,13 @@ class YouTubeDownloader:
         path = path + "%(title)s.%(ext)s"
         ydl_opts = {}
 
-        ydl_opts['format'] = format
-        ydl_opts['quiet'] = self.base_quiet
+        ydl_opts["format"] = format
+        ydl_opts["quiet"] = self.base_quiet
 
         if self.base_path is not None:
-            ydl_opts['outtmpl'] = self.base_path
+            ydl_opts["outtmpl"] = self.base_path
         else:
-            ydl_opts['outtmpl'] = path
+            ydl_opts["outtmpl"] = path
 
         try:
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -44,10 +40,7 @@ class YouTubeDownloader:
 
         info_list = []
 
-        ydl_opts = {
-            'quiet': True,
-            'skip_download': True
-        }
+        ydl_opts = {"quiet": True, "skip_download": True}
         try:
             for url in self.url_list:
                 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -55,7 +48,7 @@ class YouTubeDownloader:
                     info_list.append(info_dict)
         except Exception as e:
             print(e)
-        
+
         return info_list
 
     def download_audio(self, path="download/audios/", format="bestaudio/best") -> None:
@@ -66,14 +59,14 @@ class YouTubeDownloader:
         path = path + "%(title)s.%(ext)s"
         ydl_opts = {}
 
-        ydl_opts['format'] = format
-        ydl_opts['extractaudio'] = True
-        ydl_opts['quiet'] = self.base_quiet
+        ydl_opts["format"] = format
+        ydl_opts["extractaudio"] = True
+        ydl_opts["quiet"] = self.base_quiet
 
         if self.base_path is not None:
             ydl_opts["outtmpl"] = self.base_path
         else:
-            ydl_opts['outtmpl'] = path
+            ydl_opts["outtmpl"] = path
 
         try:
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -81,7 +74,7 @@ class YouTubeDownloader:
 
         except Exception as e:
             print(e)
-        
+
         else:
             print("Successfully downloaded.")
 
@@ -93,13 +86,13 @@ class YouTubeDownloader:
         path + path + "%(playlist)s/%(title)s.%(ext)s"
         ydl_opts = {}
 
-        ydl_opts['format'] = format
-        ydl_opts['quiet'] = self.base_quiet
+        ydl_opts["format"] = format
+        ydl_opts["quiet"] = self.base_quiet
 
         if self.base_path is not None:
             ydl_opts["outtmpl"] = self.base_path
         else:
-            ydl_opts['outtmpl'] = path
+            ydl_opts["outtmpl"] = path
 
         try:
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -110,7 +103,8 @@ class YouTubeDownloader:
         else:
             print("Successfully downloaded.")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
 
     downloader = YouTubeDownloader(
         [
